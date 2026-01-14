7233ba6c60d843a2f4de40aef4fde16525f4a96b
Guillermo Bárcena López <barcenalopezguillermo@gmail.com>
2026-01-14 09:02:51 +0100

    payment_method

---
+++ document_format/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Document format forma",
+    "category": "Report",
+    "version": "19.0.1.0.4",
+    'author': 'Guillermo Bárcena López',
+    'website': 'https://www.xtendoo.es',
+    "depends": [
+        "base",
+        "account",
+    ],
+    "license": "AGPL-3",
+    "application": True,
+    "description": """
+        Modulo para editar el formato base de los documentos
+        """,
+    "data": [
+        "views/invoice_document.xml"
+    ],
+
+    "installable": True,
+}
